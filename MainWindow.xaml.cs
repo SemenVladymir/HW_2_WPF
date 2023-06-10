e5153dade7d3dec61dd884c5ba5507095ab31b33
VladimirSemen2022 <107301979+VladimirSemen2022@users.noreply.github.com>
2023-06-10 21:53:51 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows;
+
+
+namespace HW_2_WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            Application.Current.Resources = Application.LoadComponent(new Uri("StyleResource.xaml", UriKind.Relative)) as ResourceDictionary;
+            InitializeComponent();
+        }
+    }
+}
